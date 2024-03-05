af965e1c3275141c535cafc9d203ffd7385bb2f0
jovins <jovinscoder@gmail.com>
2024-03-05 17:18:55 +0800

    update code

--- SwiftUIAnimations/SwiftUIAnimations/Animation/TwinCircleAnimation.swift
+++ SwiftUIAnimations/SwiftUIAnimations/Animation/TwinCircleAnimation.swift
@@ -2,10 +2,10 @@ import SwiftUI
 
 struct TwinCircleAnimation: View {
     
+    @State var size: CGFloat = 40
     @State var factor: CGFloat = 0
     @State private var leftColor: Color = Color.purple
     @State private var rightColor: Color = Color.purple
-    private let size: CGFloat = 40
 
     var body: some View {
         ZStack {
@@ -13,12 +13,12 @@ struct TwinCircleAnimation: View {
                 .background(leftColor)
                 .cornerRadius(size * 0.5)
                 .scaleEffect(abs(factor) * 0.3 + 1)
-                .offset(x: factor * size * 0.5, y: 0)
+                .offset(x: factor * size * 0.6, y: 0)
             VStack{}.frame(width: size, height: size, alignment: .center)
                 .background(rightColor)
                 .cornerRadius(size * 0.5)
                 .scaleEffect(abs(factor) * 0.3 + 1)
-                .offset(x: -factor * size * 0.5, y: 0)
+                .offset(x: -factor * size * 0.6, y: 0)
         }
         .frame(width: 200, height: 200)
         .onAppear {
--- SwiftUIAnimations/SwiftUIAnimations/SwiftUIAnimationsApp.swift
+++ SwiftUIAnimations/SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct SwiftUIAnimationsApp: App {
     var body: some Scene {
         WindowGroup {
-            TwinCircleAnimation()
+            TwinCircleAnimation(size: 20)
         }
     }
 }
